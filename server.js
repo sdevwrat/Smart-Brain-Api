37f797fc98a6fa4bc203f6e02ec1816ab4d0e714
Devwrat <sdevwrat@gmail.com>
2020-06-28 17:38:07 +0530

    Port added

--- server.js
+++ server.js
@@ -28,7 +28,6 @@ app.post('/register',(req,res) =>{register.handleRegister(req,res,db,bcrypt)});
 app.put('/image', (req,res) =>{image.handleImage(req,res,db)});
 app.post('/imageurl', (req,res) =>{image.handleApicall(req,res)});
 
-const PORT = process.env.PORT;
-app.listen( PORT || 3000 => {
-	console.log(`Working on port ${PORT}`);
+app.listen( process.env.PORT || 3000, () => {
+	console.log(`Working on port ${process.env.PORT}`);
 })
