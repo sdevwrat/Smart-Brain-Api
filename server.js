ab525dc14b7863044eb75dd400cbc6f426899fb1
Devwrat <sdevwrat@gmail.com>
2020-06-28 20:05:55 +0530

    added database

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-	  ssl: true,
+	  ssl: {
+	    rejectUnauthorized: false
+	  }
   }
 });
 
@@ -26,6 +28,6 @@ app.post('/register',(req,res) =>{register.handleRegister(req,res,db,bcrypt)});
 app.put('/image', (req,res) =>{image.handleImage(req,res,db)});
 app.post('/imageurl', (req,res) =>{image.handleApicall(req,res)});
 
-app.listen( 3000, () => {
-	console.log('Working on port 3000');
+app.listen( process.env.PORT || 3000, () => {
+	console.log(`Working on port ${process.env.PORT}`);
 })
